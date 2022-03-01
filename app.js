88e3bc085d3b18a05d95f929f6886213d3f691da
Abhinav Gangwar <abhinavgangwar4444@outlook.com>
2022-03-01 13:54:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// const amount = 12
+
+// if (amount < 10) {
+//     console.log('small number')
+// } else {
+//     console.log('large numner')
+// }
+
+// console.log('hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
